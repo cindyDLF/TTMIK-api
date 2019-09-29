231afff15e809d5615ea88f47ff41d401918e28f
De la fuente <delafuentecindy@gmail.com>
2019-09-29 22:15:23 +0200

    add auto script thematic

--- src/seeds/hangeul.js
+++ src/seeds/hangeul.js
@@ -2,12 +2,12 @@ import { getConnection, getRepository } from "typeorm";
 import { Exercice, Thematic } from "../entities/index";
 import { readFile } from "../utils/readFile";
 
-const initHangeul = async () => {
+const initThematic = async (thematic, arrExerciceHangeul) => {
   try {
     const thematicRepository = getRepository(Thematic);
 
     const thematicName = await thematicRepository.find({
-      where: { name: "hangeul" },
+      where: { name: thematic },
       relations: ["exercice"]
     });
 
@@ -16,11 +16,13 @@ const initHangeul = async () => {
       .select("thematic")
       .from(Thematic, "thematic")
       .leftJoinAndSelect("thematic.exercice", "exercice")
-      .where("thematic.name = :name", { name: "hangeul" })
+      .where("thematic.name = :name", { name: thematic })
       .getOne();
 
+    console.log("thematic ====>", thematicHangeul);
+
     const isExist = thematicName.map(name => {
-      if (name["name"] === "hangeul") {
+      if (name["name"] === thematic) {
         return true;
       } else {
         return false;
@@ -29,62 +31,62 @@ const initHangeul = async () => {
 
     if (isExist.length === 0 || isExist.includes(false)) {
       const hangeul = new Thematic();
-      hangeul.name = "hangeul";
+      hangeul.name = thematic;
       await thematicRepository.save(hangeul);
     } else {
       console.log("key already exists");
     }
 
-    const hangeulExercises = ["vowels", "consonants"];
+    if (thematicHangeul !== undefined) {
+      arrExerciceHangeul.forEach(async name => {
+        const exerciceEntity = await getConnection()
+          .createQueryBuilder()
+          .select("exercice")
+          .from(Exercice, "exercice")
+          .where("exercice.name = :name", { name })
+          .getOne();
 
-    hangeulExercises.forEach(async name => {
-      const exerciceEntity = await getConnection()
-        .createQueryBuilder()
-        .select("exercice")
-        .from(Exercice, "exercice")
-        .where("exercice.name = :name", { name })
-        .getOne();
-
-      if (exerciceEntity !== undefined) {
-        if (exerciceEntity.name === name) {
-          await getConnection()
-            .createQueryBuilder()
-            .delete()
-            .from(Exercice)
-            .where("name = :name", { name })
-            .execute();
-        }
-      }
-
-      const { info, data } = readFile(`../data/hangeul/${name}.json`);
-      const {
-        complete_point,
-        step,
-        point_per_step,
-        access_level,
-        exercice_type
-      } = info;
-
-      await getConnection()
-        .createQueryBuilder()
-        .insert()
-        .into(Exercice)
-        .values([
-          {
-            name,
-            complete_point,
-            step,
-            point_per_step,
-            data,
-            access_level,
-            exercice_type,
-            thematic: thematicHangeul.id
+        if (exerciceEntity !== undefined) {
+          if (exerciceEntity.name === name) {
+            await getConnection()
+              .createQueryBuilder()
+              .delete()
+              .from(Exercice)
+              .where("name = :name", { name })
+              .execute();
           }
-        ])
-        .execute();
-    });
+        }
+
+        const { info, data } = readFile(`../data/${thematic}/${name}.json`);
+        const {
+          complete_point,
+          step,
+          point_per_step,
+          access_level,
+          exercice_type
+        } = info;
+
+        await getConnection()
+          .createQueryBuilder()
+          .insert()
+          .into(Exercice)
+          .values([
+            {
+              name,
+              complete_point,
+              step,
+              point_per_step,
+              data,
+              access_level,
+              exercice_type,
+              thematic: thematicHangeul.id
+            }
+          ])
+          .execute();
+      });
+    }
   } catch (err) {
     console.log(err);
   }
 };
-export default initHangeul;
+export default initThematic;
--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,10 +1,17 @@
 import { createConnection } from "typeorm";
 import { BdConfig } from "../../config";
 
-import initHangeul from "./hangeul";
+import initThematic from "./hangeul";
+
+let thematicHangeul = "hangeul";
+let arrExerciceHangeul = ["vowels", "consonants"];
+
+let thematicVocabularies = "vocabularies";
+let arrExerciceVocabularies = ["travel"];
 
 (async () => {
   const connection = await createConnection(BdConfig);
   console.log("Database is connected");
-  initHangeul();
+  initThematic(thematicHangeul, arrExerciceHangeul);
+  initThematic(thematicVocabularies, arrExerciceVocabularies);
 })();
